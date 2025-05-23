b5e71d14130e7d2ac92b5f4954ea01dcd19b6571
Gio <sagarcia612@gmail.com>
2025-05-23 20:41:20 +0200

    first commit

---
+++ src/components/index.jsx
@@ -0,0 +1,13 @@
+import Card from "react-bootstrap/card";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/form";
+
+const Home = () => {
+  <Card>
+    <FloatingLabel>
+      <Form.Control>
+
+      </Form.Control>
+    </FloatingLabel>
+  </Card>;
+};
